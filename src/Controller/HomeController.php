e9ad2c8facb1c067d92b7289d916a51988334f0d
Amine KASMI <kasmiamine25@gmail.com>
2019-10-03 15:17:46 +0200

    Premier commit, installation et page d'acceuil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HomeController extends Controller {
+
+    /**
+     * @Route("/hello", name="hello_base")
+     * @Route("/hello/{prenom}", name="hello_prenom")
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * 
+     * Montre la page qui dit bonjour
+     *
+     * @return void
+     */
+    public function hello($prenom = "anonyme",$age = 0){
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom'=>$prenom,
+                'age'=>$age
+            ]
+        );
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+        $prenoms = ["Amine" => 24, "Sara" => 23, "Walid" => 20];
+
+        return $this->render(
+            'home.html.twig',
+            [ 
+                'title' => "Bonjour à tous",
+                'age' => 10,
+                'tableau' => $prenoms
+            ]
+        );
+    }
+}
+?>
